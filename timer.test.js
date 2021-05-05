7c698a8e7a616ba66f22ef9051ac4c85daa1831b
Julie Gelman <jgelman@hawk.iit.edu>
2021-05-05 14:50:22 -0500

    adding test case to timer test

---
+++ timer.test.js
@@ -0,0 +1,26 @@
+// ./timer.test.js
+
+const { getByText } = require("@testing-library/dom");
+require("@testing-library/jest-dom/extend-expect");
+const { JSDOM } = require("jsdom");
+const fs = require("fs");
+const path = require("path");
+
+const html = fs.readFileSync(path.resolve(__dirname, "./timer.html"), "utf8");
+
+let dom;
+let container;
+
+describe("Test for elements on timer page", () => {
+  beforeEach(() => {
+    dom = new JSDOM(html, { runScripts: "dangerously" });
+    container = dom.window.document.body;
+  });
+
+  it("Page renders a heading element", () => {
+    expect(container.querySelector("buttons")).not.toBeNull();
+    expect(
+      getByText(container, "Start")
+    ).toBeInTheDocument();
+  });
+});
